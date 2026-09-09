ac7fe70890d17c5b3e1db70f6fdd5f31d7ae2db8
EmileSpecialProducts <130773873+EmileSpecialProducts@users.noreply.github.com>
2026-09-09 19:44:27 +0200

    Updated the Async handling of the http request

--- src/Http.cpp
+++ src/Http.cpp
@@ -1,8 +1,10 @@
 
 #include "canary.hpp"
 #include <lwip/sockets.h>
-WiFiServer HttpServer(443);
-WiFiServer HttpaServer(8080);
+AsyncWebServer *HttpServer=nullptr;
+AsyncWebServer *HttpsServer=nullptr;
+AsyncWebServer *HttpaServer=nullptr;
+
 String HttpHookweb = "";
 String HttpsHookweb = "";
 String HttpaHookweb = "";
@@ -10,6 +12,48 @@ bool Httpenabled = false;
 bool Httpsenabled = false;
 bool Httpaenabled = false;
 // https://github.com/me-no-dev/ESPAsyncTCP/blob/master/examples/ClientServer/Server/Server.ino
+
+void HandelNotFount(AsyncWebServerRequest *request, String Directory, String webHook)
+    { 
+        String ip = request->client()->remoteIP().toString();
+        String payload = request->url();//dumpBytes(request->client());
+        int port = request->client()->localPort();
+        logCommand(webHook,ip, port, payload);
+        
+        debugf("HandelNotFount %s , Method =%s\n", request->url().c_str(), request->methodToString());
+        debugf("%s\n", (Directory + request->url()).c_str());
+        if (request->method() == AsyncWebRequestMethod::HTTP_GET)
+        {
+            if (LittleFS.exists(Directory + request->url())) // exists will give a error in the error log see: https://github.com/espressif/arduino-esp32/issues/7615
+            {
+                request->send(LittleFS, Directory + request->url(), String(), false);
+            }
+            else
+            {
+            if (request->url() == "/error404.html")
+                    request->send( 404, "text/html", error404_html, sizeof(error404_html) - 1);
+            else 
+                    request->redirect("/error404.html");
+            }
+        }
+        else if (request->method() == AsyncWebRequestMethod::HTTP_POST)
+        {
+            request->send( 404, "text/html", error404_html, sizeof(error404_html) - 1);
+        }
+    }
+
+void HandelNotFountHTTP(AsyncWebServerRequest *request)
+{
+  HandelNotFount(request,"/http",HttpHookweb);
+}
+void HandelNotFountHTTPS(AsyncWebServerRequest *request)
+{
+  HandelNotFount(request,"/https",HttpsHookweb);
+}
+void HandelNotFountHTTPA(AsyncWebServerRequest *request)
+{
+  HandelNotFount(request,"/httpa",HttpaHookweb);
+}
 
 void setup_http()
 {
@@ -20,39 +64,35 @@ void setup_http()
     HttpHookweb=Settingsdoc["Http"]["url"] | "";
     HttpsHookweb=Settingsdoc["Https"]["url"] | "";
     HttpaHookweb=Settingsdoc["Httpa"]["url"] | "";
-    if(Httpsenabled)HttpServer.begin();
-    if(Httpaenabled)HttpaServer.begin();
+  //DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*"); 
+  //DefaultHeaders::Instance().addHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD");
+  //DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "content-type");
+
+  if(Httpenabled)
+  {
+    HttpServer=new AsyncWebServer(80);
+    HttpServer->on("/", AsyncWebRequestMethod::HTTP_GET, [](AsyncWebServerRequest *request)
+    { request->redirect("/index.html"); });
+    HttpServer->onNotFound(HandelNotFountHTTP);
+    HttpServer->begin();
+  } 
+  if(Httpsenabled)
+  {
+    HttpsServer=new AsyncWebServer(443);
+    HttpsServer->on("/", AsyncWebRequestMethod::HTTP_GET, [](AsyncWebServerRequest *request)
+    { request->redirect("/index.html"); });
+    HttpsServer->onNotFound(HandelNotFountHTTPS);
+    HttpsServer->begin();
+  }
+  if(Httpaenabled)
+  {
+    HttpaServer=new AsyncWebServer(8080);
+    HttpaServer->on("/", AsyncWebRequestMethod::HTTP_GET, [](AsyncWebServerRequest *request)
+    { request->redirect("/index.html"); });
+    HttpaServer->onNotFound(HandelNotFountHTTPA);
+    HttpaServer->begin();
+  }
 }
 void loop_http()
 {
-if(Httpsenabled)
-{
-    /* ---------- Http on 443 (no TLS) ---------- */
-    if (WiFiClient c = HttpServer.accept())
-    handleBannerGrab(HttpsHookweb,
-      c, 443,
-      "HTTP/1.1 200 OK\r\n"
-      "Server: Apache/2.4.52 (Debian)\r\n"
-      "Content-Type: text/html\r\n"
-      "Content-Length: 44\r\n\r\n"
-      "<html><body><h1>It works!</h1></body></html>");
-}
-if(Httpaenabled) 
-  {
-    /* ---------- Http alternative (no TLS) ---------- */
-    if (WiFiClient c = HttpaServer.accept())
-    {
-    handleBannerGrab(HttpaHookweb,
-      c, 8080,
-      "HTTP/1.1 200 OK\r\n"
-      "Server: Apache/2.4.52 (Debian)\r\n"
-      "Content-Type: text/html\r\n"
-      "Content-Length: 44\r\n\r\n"
-      "<html><body><h1>It works!</h1></body></html>");
-      //c.clear();
-      c.stop();
-      //HttpaServer.close();
-      //closesocket(c);
-    }
-  }
 }
