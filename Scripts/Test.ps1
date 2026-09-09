db57185b0c13ead5964de34b3bb90ea631e16db3
EmileSpecialProducts <130773873+EmileSpecialProducts@users.noreply.github.com>
2026-09-09 20:52:35 +0200

    Updated the TestScript

--- Scripts/Test.ps1
+++ Scripts/Test.ps1
@@ -1,6 +1,17 @@
-$Response = Invoke-WebRequest -Uri espredcanary.local:8080
-write-host $Response.StatusCode 
+param(
+	[string]$ESPredCanaryHost = "espredcanary.local"
+)
 
-$Response = Invoke-WebRequest -Uri espredcanary.local:443
-write-host $Response.StatusCode 
+Test-Connection -ComputerName $ESPredCanaryHost -Count 1
+write-host  $ESPredCanaryHost
+$Response = Invoke-WebRequest -UseBasicParsing -Uri $ESPredCanaryHost
+write-host "Port 80: $($Response).StatusCode"
 
+write-host  $ESPredCanaryHost":8080"
+$Response = Invoke-WebRequest -UseBasicParsing -Uri $ESPredCanaryHost":8080"
+write-host "Port 8080: $($Response).StatusCode"
+
+write-host  $ESPredCanaryHost":443"
+$Response = Invoke-WebRequest -UseBasicParsing -Uri $ESPredCanaryHost":443"
+write-host "Port 443: $($Response).StatusCode"
+Invoke-WebRequest -UseBasicParsing -Uri "espredcanary.local:443"
